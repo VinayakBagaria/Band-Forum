adaa1da89c0fb91fec04695c1e629b9b4bbb284e
Vinayak Bagaria <workvinayak95@gmail.com>
2017-03-22 10:54:38 +0530

    corrected procfile

--- music/settings.py
+++ music/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$@tc6vi)2w$%s%awrd3i+@%$pslr8hx&e)oj$bp9hf$)x2k415'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",]
 
 
 
