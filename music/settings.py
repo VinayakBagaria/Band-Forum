3f75f1017058d6ce70389cd20fdba02d4ebf605c
Vinayak Bagaria <workvinayak95@gmail.com>
2017-03-22 10:24:50 +0530

    static root

--- music/settings.py
+++ music/settings.py
@@ -136,6 +136,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 
+STATIC_ROOT = (os.path.join(BASE_DIR,'staticfiles'),)
+
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'bundles/',
--- music/urls.py
+++ music/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
     url(r'^band/', include('band.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
