578bd6f058b8ce1146ff85d104a5676dd54dd712
Vinayak Bagaria <workvinayak95@gmail.com>
2017-03-22 15:22:32 +0530

    Removed key

--- music/settings.py
+++ music/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$@tc6vi)2w$%s%awrd3i+@%$pslr8hx&e)oj$bp9hf$)x2k415'
+SECRET_KEY = #Some secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
