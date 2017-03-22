67864e51fb20ebc21134634da31890172a66f706
Vinayak Bagaria <workvinayak95@gmail.com>
2017-03-22 10:57:31 +0530

    static root

--- music/urls.py
+++ music/urls.py
@@ -17,8 +17,9 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from . import views
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
     url(r'^band/', include('band.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
